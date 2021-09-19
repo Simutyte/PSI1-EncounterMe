6919019f1b38bc377a70e105c9f971d246f07679
Povilas Slivinskas <slivinskas.povilas@@gmail.com>
2021-09-19 20:36:42 +0300

    Added maps button
    Maps button in MainPage, Maps moved to new file Map.xaml

--- EncounterMe/EncounterMe/MainPage.xaml.cs
+++ EncounterMe/EncounterMe/MainPage.xaml.cs
@@ -14,5 +14,10 @@ namespace EncounterMe
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Map());
+        }
     }
 }
