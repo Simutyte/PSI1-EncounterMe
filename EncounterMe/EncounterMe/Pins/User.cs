ef7f87be62edaecadc0c8a440cd3719eff5c0f87
Rugilė Simutytė <rugile.simutyte@gmail.com>
2021-09-27 17:37:44 +0300

    Changing login page and adding some features

---
+++ EncounterMe/EncounterMe/Pins/User.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncounterMe.Pins
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public User() { }
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        public bool checkInformation()
+        {
+            if(!String.IsNullOrEmpty(Username) && !String.IsNullOrEmpty(Password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+ 
+    }
+}
