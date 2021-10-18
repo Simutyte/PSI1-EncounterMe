cde27c916045260093cd5408e23d91c5c6b9a45a
rusi7334 <rugile.simutyte@mif.stud.vu.lt>
2021-10-18 13:47:51 +0300

    fixing gif and added extension method

---
+++ EncounterMe/EncounterMe/Pins/AddPin.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Maps;
+
+namespace EncounterMe.Pins
+{
+    public static class AddPin
+    {
+
+        public static void CreateAPin(this MapPin mapPin)
+        {
+            mapPin.pin = new Pin()
+            {
+                Label = mapPin.name,
+                Address = mapPin.address,
+                Type = PinType.Place,
+                Position = new Position(mapPin.location.Latitude, mapPin.location.Longitude)
+            };
+        }
+    }
+}
