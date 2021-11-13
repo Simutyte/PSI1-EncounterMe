bebf49a07837670227cde881f73f6c5fe868006d
Rosita Raišuotytė <rosita.raisuotyte@mif.stud.vu.lt>
2021-11-13 21:48:58 +0200

    Recovered IO file, because it has generics methods

---
+++ EncounterMe/EncounterMe/Pins/IO.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EncounterMe
+{
+    static class IO
+    {
+        //public static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = true)
+        //{
+        //    using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
+        //    {
+        //        var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+        //        binaryFormatter.Serialize(stream, objectToWrite);
+        //    }
+        //}
+
+        //public static List<T> ReadFromBinaryFile<T>(string filePath)
+        //{
+        //    var list = new List<T>();
+
+        //    try
+        //    {
+        //        using (Stream stream = File.Open(filePath, FileMode.Open))
+        //        {
+        //            var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+        //            while (stream.Position < stream.Length)
+        //            {
+        //                T obj = (T)binaryFormatter.Deserialize(stream);
+        //                list.Add(obj);
+        //            }
+        //            return list;
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return null;
+        //    }
+        //}
+    }
+}
